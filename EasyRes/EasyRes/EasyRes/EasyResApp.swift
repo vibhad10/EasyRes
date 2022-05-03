8e4b870df3e2740a638323e9be158843731b7f24
Vibha Dabholkar <87840934+vibhad10@users.noreply.github.com>
2022-05-02 17:55:31 -0700

    Add files via upload

---
+++ EasyRes/EasyRes/EasyRes/EasyResApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyResApp.swift
+//  EasyRes
+//
+//  Created by Jane on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct EasyResApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
